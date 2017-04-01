81cdf72705ca381c2d8fc5d4abed5b826721f7c5
Susha Dore <susha.dore@gmail.com>
2017-03-31 22:55:47 -0700

    create Pizza object & establish functionability for checkboxes and radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,28 @@
-function Pizza (topping, size) {
+function Pizza (topping, size){
   this.topping = topping;
   this.size = size;
 }
 
-$(function() {
-  $("button").submit(function(event) {
+var pizzaToppings=[];
+
+$(function(){
+  $("form").submit(function(event){
     event.preventDefault();
 
-    $("input:checkbox[name=free]:checked").each(function() {
-      var freeToppings = $(this).val();
-      alert(freeToppings);
+    $("input:checkbox[name=topping]:checked").each(function(){
+      var inputToppings = $(this).val();
+      pizzaToppings.push(inputToppings);
     });
-    $("input:checkbox[name=meat]:checked").each(function() {
-      var meatToppings = $(this).val();
-    });
-    $("input:checkbox[name=veggie]:checked").each(function() {
-      var vegToppings = $(this).val();
-    });
+
+    // $("input:checkbox[name=meat]:checked").each(function(){
+    //   var meatToppings = $(this).val();
+    //   pizzaToppings.push(meatToppings);
+    // });
+    // $("input:checkbox[name=veggie]:checked").each(function(){
+    //   var vegToppings = $(this).val();
+    //   pizzaToppings.push(vegToppings);
+    // });
+    var pizzaSize = parseInt($("input:radio[name=size]:checked").val());
+  
   });
 });
