690808a15ccad2f736b81e1a03a4020c0415d33d
Susha Dore <susha.dore@gmail.com>
2017-04-01 00:37:08 -0700

    edit prototype to adjust for both pizza size and toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,25 @@
-function Pizza (size, topping){
-  this.topping = topping;
+function Pizza (size){
   this.size = size;
+  this.toppings = [];
 }
 
 Pizza.prototype.sizePrice=function(){
-  return 10 + this.size;
+  return 10 + this.size + this.toppings.length;
 }
- var toppings = [];
+
 $(function(){
   $("form").submit(function(event){
     event.preventDefault();
 
     var pizzaSize = parseInt($("input:radio[name=size]:checked").val());
-
+    var newPizza = new Pizza (pizzaSize);
 
     $("input:checkbox[name=topping]:checked").each(function(){
       var inputToppings = $(this).val();
-      toppings.push(inputToppings);
-      console.log(toppings);
+      newPizza.toppings.push(inputToppings);
     });
-    var newPizza = new Pizza (pizzaSize, toppings);
-    // $("input:checkbox[name=meat]:checked").each(function(){
-    //   var meatToppings = $(this).val();
-    //   pizzaToppings.push(meatToppings);
-    // });
-    // $("input:checkbox[name=veggie]:checked").each(function(){
-    //   var vegToppings = $(this).val();
-    //   pizzaToppings.push(vegToppings);
-    // });
 
+    $("#pizza-toppings").text(newPizza.toppings);
     $("#price").text(newPizza.sizePrice());
     $("p").show();
     console.log(newPizza);
